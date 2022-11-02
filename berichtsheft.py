d48a9468ccae55ccf67189435b566cef4a1ce50e
ByteKrieger <info@bytekrieger.de>
2022-11-02 21:27:30 +0100

    grundfunktion erstellt berichtsheft.py

--- berichtsheft.py
+++ berichtsheft.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import datetime
+import time
+
+heute = datetime.date.today()
+print(heute)
+x = 0
+
+datei = open("berichtsheft.txt", "a")
+datei.write("\n")
+datei.write(str(heute))
+datei.write("\n")
+datei.close()
+
+while x != 1:
+    eingabe = input("Bitte gib die nächste Tätigkeit ein: ")
+    if eingabe == "x":
+        break
+    else:
+        datei = open("berichtsheft.txt", "a")
+        datei.write(eingabe)
+        datei.write("\n")
+        datei.close()
+
+
+print("Das Berichtsheft wurde erfolgreich bearbeitet.")
+time.sleep(2)
